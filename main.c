8933041a133eb6b8f26c0480e00666275f5a249b
Daniel <danobentey@gmail.com>
2023-11-15 14:50:21 -0800

    simple_shell_1

--- main.c
+++ main.c
@@ -1,24 +1,45 @@
-
-#include "lists.h"
+#include "main.h"
 
 /**
- * get_nodeint_at_index - returns the node at a certain index in a linked list
- * @head: first node in the linked list
- * @index: index of the node to return
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: pointer to the node we're looking for, or NULL
+ * Return: 0 on success, 1 on error
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+int main(int ac, char **av)
 {
-	unsigned int i = 0;
-	listint_t *temp = head;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	while (temp && i < index)
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
 	{
-		temp = temp->next;
-		i++;
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
 	}
-
-	return (temp ? temp : NULL);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
 
